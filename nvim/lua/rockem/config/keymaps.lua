1ef3ee10319a5dda66eefc7a01e39169fe34a0ba
Eli Segal <eli.segal@gmail.com>
2024-10-22 20:23:20 +0300

    Added more plugins to nvim

--- nvim/lua/rockem/config/keymaps.lua
+++ nvim/lua/rockem/config/keymaps.lua
@@ -1,9 +1,18 @@
 -- set leader key to space
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local map = vim.keymap.set -- for conciseness
 
+-- Buffer
 map("n", "<C-Tab>", ":b#<CR>")
 
 -- Git 
 map("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Preview git changes" })
+
+-- Fuzzy find
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files in cwd" })
+map("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Find recent files" })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
+map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
+map("n", "<leader>fc", "<cmd>Telescope find_files search_dirs=~/.config/nvim<CR>", { desc = "Find config files" })
--- nvim/lua/rockem/plugins/telescope.lua
+++ nvim/lua/rockem/plugins/telescope.lua
@@ -17,12 +17,5 @@ return {
       telescope.load_extension("fzf")
       telescope.load_extension("file_browser")
 
-      local map = vim.keymap.set
-
-      map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files in cwd" })
-      map("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Find recent files" })
-      map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
-      map("n", "<leader>fc", "<cmd>Telescope find_files search_dirs=~/.config/nvim<CR>", { desc = "Find config files" })
-
     end,
 }
