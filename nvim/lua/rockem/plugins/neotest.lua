21645f83cb65df553e9bdc4ec5d5e654148d1b0f
Eli Segal <eli.segal@gmail.com>
2024-11-03 12:03:30 +0200

    Added bringthemhome label

--- nvim/lua/rockem/plugins/neotest.lua
+++ nvim/lua/rockem/plugins/neotest.lua
@@ -8,15 +8,30 @@ return {
 		"sidlatau/neotest-dart",
 	},
 	config = function()
+		-- local device_id = vim.g.flutter_device_id
 		require("neotest").setup({
 			adapters = {
-
 				require("neotest-dart")({
 					command = "flutter",
 					use_lsp = true,
 				}),
 			},
 			log_level = vim.log.levels.DEBUG,
+			timeout = 30000,
+			output_panel = {
+				open_on_run = true,
+			},
+			consumers = {
+				live_output = function(client)
+					client.listeners.run = function(_, _)
+						-- Open the live output pane when a test is running
+						require("neotest").output_panel.open({
+							enter = false,
+							follow = true, -- Keep it updated with real-time output
+						})
+					end
+				end,
+			},
 		})
 	end,
 }
