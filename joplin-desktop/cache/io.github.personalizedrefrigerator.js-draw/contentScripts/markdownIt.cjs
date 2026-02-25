9e7f64a98a0493bc847a4bbabe510471d32d7f0e
Eli Segal <eli.segal@gmail.com>
2026-02-26 00:41:20 +0200

    Delete redundant dirs

--- joplin-desktop/cache/io.github.personalizedrefrigerator.js-draw/contentScripts/markdownIt.cjs
+++
@@ -1,9 +0,0 @@
-
-exports.default = () => {
-	return {
-		plugin: (_markdownIt, _options) => {},
-		assets: () => {
-			return [{ name: 'markdownIt.css' }, { name: 'markdownIt-content.js' }];
-		},
-	};
-};
